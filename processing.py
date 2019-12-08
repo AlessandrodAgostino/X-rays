6c32e37c7e208a13252e1fe82faf786d0f9d3350
alessandro <alessandr.dagostino2@studio.unibo.it>
2019-12-08 12:12:59 +0100

    first analysis

--- processing.py
+++ processing.py
@@ -1,29 +1,27 @@
 import numpy as np
 import matplotlib.pylab as plt
-from os.path import join as pj
 import seaborn as sns
 
-Ladder_img = np.fromfile(pj('Aquisition 4','ladder_120kV_micoA4100_16average_10frate.sdt'), dtype='uint16', sep="")
-Ladder_img = np.reshape(Ladder_img, (1920, 1536))
+"""
+Measuring average gray levels in the same ROI of different images took with
+different current intensities.
+Analysing always the same ROI for every image.
+From every image the "dark" image has been subtracted (image took with x-ray
+source tourned off).
+The linear regression plot in drawn -> perfectly linear relation.
+"""
 
-I0_img = np.fromfile(pj('Aquisition 4','I0_120kV_micoA4100_16average_10frate.sdt'), dtype='uint16', sep="")
-I0_img = np.reshape(I0_img, (1920, 1536))
-
-dark = np.fromfile('dark.sdt', dtype='uint16', sep="")
-dark = np.reshape(dark, (1920, 1536))
-
-#Should increase the contrast!
-plt.imshow((Ladder_img - dark)/I0_img)
-
-
-#Measuring average graylevel
+#%%
+#different ampere values -> filenames parsing
 ampere = np.linspace(0,6, 7, dtype=np.int16)
 ampere = ampere*5 + 10
 print(ampere)
 
+#loading the dark image
 dark = np.fromfile('dark.sdt', dtype='uint16', sep="")
 dark = np.reshape(dark, (1920, 1536))
 
+#loading and analysing the same ROI on every 16 average frame image
 average_gl_16frame = np.zeros(7)
 for n,amp in enumerate(ampere):
   filename= f'microA{amp}00_average16.sdt'
@@ -32,6 +30,7 @@ for n,amp in enumerate(ampere):
   img = img -dark
   average_gl_16frame[n]= np.average(img[200:800, 200:800])
 
+#loading and analysing the same ROI on every 1 frame image
 average_gl_1frame = np.zeros(7)
 for n,amp in enumerate(ampere):
   filename= f'microA{amp}00.sdt'
@@ -40,5 +39,12 @@ for n,amp in enumerate(ampere):
   img = img -dark
   average_gl_1frame[n]= np.average(img[200:800, 200:800])
 
-plt.scatter(ampere, average_gl_16frame)
-plt.scatter(ampere, average_gl_1frame)
+#%%
+#Plotting the results
+fig = plt.figure()
+sns.regplot(ampere, average_gl_16frame)
+# sns.regplot(ampere, average_gl_16frame) #superimposing perfectly
+fig.suptitle('GL - Current', fontsize=20)
+plt.xlabel('Current (mA)', fontsize=18)
+plt.ylabel('Grey Level', fontsize=16)
+fig.savefig('Results/GL_current_linreg.jpg')
